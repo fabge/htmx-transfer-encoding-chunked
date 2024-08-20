311a479748690864af685f0bbe736c138207c886
Fabian <fa.geiger@enbw.com>
2024-08-20 20:15:34 +0200

    fix unresponsive form after first submit

--- transfer-encoding-chunked.js
+++ transfer-encoding-chunked.js
@@ -1,62 +1,54 @@
-(function(){
-  /** @type {import("../htmx").HtmxInternalApi} */
-  var api;
-  var lastResponseLength = 0;
-  var isComplete = false;
+(function () {
+    /** @type {import("../htmx").HtmxInternalApi} */
+    var api;
+    var accumulatedResponse = '';
 
-  htmx.defineExtension('chunked-transfer', {
-      init: function(apiRef) {
-          api = apiRef;
-      },
+    htmx.defineExtension('chunked-transfer', {
+        init: function (apiRef) {
+            api = apiRef;
+        },
 
-      onEvent: function(name, evt) {
-          if (name === "htmx:beforeRequest") {
-              var xhr = evt.detail.xhr;
-              var elt = evt.detail.elt;
+        onEvent: function (name, evt) {
+            if (name === "htmx:beforeRequest") {
+                var xhr = evt.detail.xhr;
+                var elt = evt.detail.elt;
 
-              isComplete = false;
+                xhr.onprogress = function () {
+                    var isChunked = xhr.getResponseHeader("Transfer-Encoding") === "chunked" ||
+                        xhr.getResponseHeader("X-Transfer-Encoding") === "chunked";
+                    if (!isChunked) return;
 
-              xhr.onprogress = function() {
-                  if (isComplete) return;
+                    var response = xhr.response;
+                    var newChunk = response.substring(accumulatedResponse.length);
 
-                  var isChunked = xhr.getResponseHeader("Transfer-Encoding") === "chunked" ||
-                                xhr.getResponseHeader("X-Transfer-Encoding") === "chunked";
-                  if (!isChunked) return;
+                    if (newChunk.trim() === "") return;
 
-                  var response = xhr.response;
-                  var newChunk = response.substring(lastResponseLength);
-                  lastResponseLength = response.length;
+                    accumulatedResponse += newChunk;
 
-                  if (newChunk.trim() === "") return;
+                    api.withExtensions(elt, function (extension) {
+                        if (extension.transformResponse) {
+                            newChunk = extension.transformResponse(newChunk, xhr, elt);
+                        }
+                    });
 
-                  api.withExtensions(elt, function(extension) {
-                      if (extension.transformResponse) {
-                          newChunk = extension.transformResponse(newChunk, xhr, elt);
-                      }
-                  });
+                    var swapSpec = api.getSwapSpecification(elt);
+                    var target = api.getTarget(elt);
 
-                  var swapSpec = api.getSwapSpecification(elt);
-                  var target = api.getTarget(elt);
+                    htmx.swap(target, newChunk, {
+                        swapStyle: swapSpec.swapStyle,
+                        swapDelay: swapSpec.swapDelay,
+                        settleDelay: swapSpec.settleDelay,
+                        ignoreTitle: true,
+                        appendOnly: true
+                    });
+                };
 
-                  htmx.swap(target, newChunk, {
-                      swapStyle: swapSpec.swapStyle,
-                      swapDelay: swapSpec.swapDelay,
-                      settleDelay: swapSpec.settleDelay,
-                      ignoreTitle: true,
-                      appendOnly: true,
-                      contextElement: elt
-                  });
-              };
+                xhr.onloadend = function () {
+                    accumulatedResponse = '';
+                };
 
-              xhr.onload = function() {
-                  isComplete = true;
-              };
-
-              xhr.onloadend = function() {
-                  lastResponseLength = 0;
-                  isComplete = false;
-              };
-          }
-      }
-  });
+                evt.detail.shouldSwap = false;
+            }
+        }
+    });
 })();
